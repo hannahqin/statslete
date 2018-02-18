9dea7d2f0cae249a0c3a60d1c3146d4f4b80fa1f
qinh <hqin@umich.edu>
2018-02-17 20:59:28 -0500

    fixed Content Security Policy error that was blocking API call from returning

--- scripts/statslete.js
+++ scripts/statslete.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
+    $('.result').text('hello world');
+
     $.ajax({
-        url: "http://stats.nba.com/stats/commonplayerinfo/?playerid=1628425",
+        url: "https://stats.nba.com/stats/commonplayerinfo/?playerid=1628425",
         dataType: "jsonp",
+        crossDomain: true,
         success: function (data) {
+            // log api call results and then print them to chrome extension
             console.log(data);
+            $('.results').text(JSON.stringify(data));
         }
-    });
+    }); 
 });
