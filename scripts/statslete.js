39a708e734774f1c7518229b4be6cf96d8df3586
qinh <hqin@umich.edu>
2018-03-05 18:19:26 -0500

    added player fantasy scripts to main script file

--- scripts/statslete.js
+++ scripts/statslete.js
@@ -54,6 +54,26 @@ $(document).ready(function() {
             // $('.career-stats').text(JSON.stringify(data));
         }
     });
+
+
+    $.ajax({
+        url: 'http://stats.nba.com/stats/playerdashboardbygeneralsplits/?PlayerID=2200&MeasureType=Base&PerMode=PerGame&PlusMinus=N&PaceAdjust=N&Rank=N&Season=2017-18&SeasonType=Regular%20Season&Outcome=&Location=&SeasonSegment=&DateFrom=&DateTo=&VsConference=&VsDivision=&Month=0&OpponentTeamID=0&GameSegment=&Period=0&LastNGames=0',
+        dataType: "jsonp",
+        crossDomain: true,
+        success: function(data) {
+            var infoNeeded = ["PTS", "REB", "AST", "STL", "BLK", "TOV"]
+            var scoringMultiplier = [1,1.2,1.5,3,3,-1]
+            var fantasyScore = 0.0
+
+            for(i = 0; i < infoNeeded.length; i++){
+
+                var stat = data["resultSets"][0]["rowSet"][0][data["resultSets"][0]["headers"].indexOf(infoNeeded[i])]
+                console.log(infoNeeded[i] + " " + stat)
+                fantasyScore += parseFloat(stat) * scoringMultiplier[i]
+            }
+            console.log("Avg fantasy pts: " + fantasyScore)
+        }
+    })
 });
 
 function getPlayerPhoto(firstname, lastname) {
